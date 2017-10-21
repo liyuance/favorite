3753e19e08861273c147dedd929d358a0c4faeb8
liyuance <liyuance@gmail.com>
2017-10-22 01:12:50 +0800

    upload setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='favorite',
+    version='0.1',
+    description=(
+        'A tool for collect your favoriter commands'
+    ),
+    long_description=open('README.rst').read(),
+    author='liyuance',
+    author_email='liyuance@gmail.com',
+    maintainer='liyuance',
+    maintainer_email='liyuance@gmail.com',
+    license='BSD License',
+    platforms=["all"],
+    url='https://github.com/liyuance/favorite',
+    classifiers=[
+        'Programming Language :: Python'
+    ],
+)
